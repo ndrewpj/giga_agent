c3e9f9d05db324de06ffd71d25e12130b5bdf7af
mikelarg <murtazkorpaz@gmail.com>
2025-08-13 17:23:54 +0300

    - fix tool call messages filtering

--- backend/graph/giga_agent/utils/messages.py
+++ backend/graph/giga_agent/utils/messages.py
@@ -1,13 +1,17 @@
-from langchain_core.messages import ToolMessage
+from langchain_core.messages import ToolMessage, AIMessage
 
 
 def filter_tool_messages(messages):
     filtered_messages = []
     for idx, msg in enumerate(messages):
         if isinstance(msg, ToolMessage):
-            if idx - 1 > 0 and not messages[idx - 1].additional_kwargs.get(
-                "function_call"
-            ):
+            if idx - 1 <= 0:
+                continue
+            ai_message_tool_called = (
+                messages[idx - 1].additional_kwargs.get("function_call")
+                or messages[idx - 1].tool_calls
+            )
+            if not ai_message_tool_called:
                 continue
         filtered_messages.append(msg)
     return filtered_messages
